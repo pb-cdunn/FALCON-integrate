14f130395ab4ceb427582b89a547b0d2b9152f7e
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-03-28 16:18:17 -0700

    Add meson to module

--- module.sh
+++ module.sh
@@ -1,6 +1,10 @@
 module purge
+module load git/2.14.1
 module load smrttools/develop
 module load python/2 gcc ccache
 # There was a bug in Nim 0.18.1.
 module load nim/0.19.4
-module load git/2.14.1
+module load meson ninja
+module load zlib
+
+# You may need to re-add $PYTHONUSERBASE/bin to the front of $PATH.
